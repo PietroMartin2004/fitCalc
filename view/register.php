451f9c3305243c4a3ee418d5099b3328c8e7f514
PietroMartin2004 <pietrolimamartin2004@gmail.com>
2025-06-17 16:40:09 -0300

    feat: Instalação do Composer/autoload + PDO

--- view/register.php
+++ view/register.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="../templates/assets/css/register.css">
+    <link rel="stylesheet" href="../template/assets/css/register.css">
     <title>FitCalc | Criar Conta</title>
 </head>
 
